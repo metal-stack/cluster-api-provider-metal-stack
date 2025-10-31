c473f2283bc615e2924074719ab84fbb7c9e427b
Simon Mayer <Simon.Mayer@x-cellent.com>
2025-10-31 15:18:50 +0100

    Rename to `E2E_DEFAULT_FLAVOR`

--- test/e2e/frmwrk/shared_context.go
+++ test/e2e/frmwrk/shared_context.go
@@ -103,7 +103,7 @@ func withDefaultEnvironment() Option {
 		e2e.Environment.kubernetesVersions = strings.Split(e2e.envOrVar("E2E_KUBERNETES_VERSIONS"), ",")
 		e2e.Environment.controlPlaneMachineImagePrefix = e2e.envOrVar("E2E_CONTROL_PLANE_MACHINE_IMAGE_PREFIX")
 		e2e.Environment.workerMachineImagePrefix = e2e.envOrVar("E2E_WORKER_MACHINE_IMAGE_PREFIX")
-		e2e.Environment.Flavor = e2e.envOrVar("E2E_FLAVOR")
+		e2e.Environment.Flavor = e2e.envOrVar("E2E_DEFAULT_FLAVOR")
 
 		_ = e2e.envOrVar("CONTROL_PLANE_MACHINE_SIZE")
 		_ = e2e.envOrVar("WORKER_MACHINE_SIZE")
