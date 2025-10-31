e53bc2ece81625eb224c6f77a84a0653f090a0a4
Simon Mayer <Simon.Mayer@x-cellent.com>
2025-10-31 17:44:03 +0100

    Use `flavor` for upgrade

--- test/e2e/frmwrk/cluster_upgrade_kubernetes_test.go
+++ test/e2e/frmwrk/cluster_upgrade_kubernetes_test.go
@@ -3,37 +3,53 @@ package frmwrk
 import (
 	"context"
 	"fmt"
+	. "github.com/onsi/ginkgo/v2" //nolint:staticcheck
 	"k8s.io/utils/ptr"
 	"os"
-	"strings"
 
-	"sigs.k8s.io/cluster-api/test/framework/clusterctl"
-
-	. "github.com/onsi/ginkgo/v2" //nolint:staticcheck
+	capi_e2e "sigs.k8s.io/cluster-api/test/e2e"
 )
 
 var _ = Describe("Upgrade Kubernetes Cluster Version", Label("upgrade"), func() {
 	ctx := context.TODO()
-	fromKubernetesVersion := strings.Split(os.Getenv("KUBERNETES_VERSION_UPGRADE_FROM"), ",")
-	toKubernetesVersion := strings.Split(os.Getenv("KUBERNETES_VERSION_UPGRADE_TO"), ",")
+	fromKubernetesVersion := os.Getenv("KUBERNETES_VERSION_UPGRADE_FROM")
+	toKubernetesVersion := os.Getenv("KUBERNETES_VERSION_UPGRADE_TO")
 
-	It(fmt.Sprintf("from %d to %d", fromKubernetesVersion, toKubernetesVersion), func() {
+	ec := e2eCtx.NewE2ECluster(ClusterConfig{
+		SpecName:                 "cluster-upgrade",
+		NamespaceName:            "cluster-upgrade",
+		ClusterName:              "cluster-upgrade",
+		KubernetesVersion:        fromKubernetesVersion, //TBD
+		ControlPlaneMachineImage: os.Getenv("E2E_CONTROL_PLANE_MACHINE_IMAGE_PREFIX") + fromKubernetesVersion,
+		ControlPlaneMachineCount: 1,
+		WorkerMachineImage:       os.Getenv("E2E_WORKER_MACHINE_IMAGE_PREFIX") + fromKubernetesVersion,
+		WorkerMachineCount:       1,
+	})
+
+	ec.SetupMetalStackPreconditions(ctx)
+	ec.SetupNamespace(ctx)
+
+	DeferCleanup(func() {
+		ec.Dump(context.Background())
+	})
+
+	It(fmt.Sprintf("from %s to %s", fromKubernetesVersion, toKubernetesVersion), func() {
 		cfg := e2eCtx.E2EConfig.DeepCopy()
 		cfg.Variables["KUBERNETES_VERSION_UPGRADE_FROM"] = e2eCtx.envOrVar("KUBERNETES_VERSION_UPGRADE_FROM")
 		cfg.Variables["KUBERNETES_VERSION_UPGRADE_TO"] = e2eCtx.envOrVar("KUBERNETES_VERSION_UPGRADE_TO")
 		cfg.Variables["KUBETEST_CONFIGURATION"] = e2eCtx.envOrVar("KUBETEST_CONFIGURATION")
 
-		clusterUpgrade(ctx, e2eCtx, func() ClusterUpgradeSpecInput {
-			return ClusterUpgradeSpecInput{
+		capi_e2e.ClusterUpgradeConformanceSpec(ctx, func() capi_e2e.ClusterUpgradeConformanceSpecInput {
+			return capi_e2e.ClusterUpgradeConformanceSpecInput{
 				E2EConfig:                cfg,
 				ClusterctlConfigPath:     e2eCtx.Environment.ClusterctlConfigPath,
 				BootstrapClusterProxy:    e2eCtx.Environment.Bootstrap,
 				ArtifactFolder:           e2eCtx.Environment.artifactsPath,
-				SkipCleanup:              true,
+				SkipCleanup:              false,
 				SkipConformanceTests:     true,
-				ControlPlaneWaiters:      clusterctl.ControlPlaneWaiters{},
 				ControlPlaneMachineCount: ptr.To[int64](1),
 				WorkerMachineCount:       ptr.To[int64](1),
+				Flavor:                   ptr.To(e2eCtx.Environment.Flavor),
 			}
 		})
 	})
