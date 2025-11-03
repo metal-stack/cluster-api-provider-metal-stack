4ac6088fe353865eb51e7f4866175adc346c20c5
Simon Mayer <Simon.Mayer@x-cellent.com>
2025-11-03 11:09:50 +0100

    Refactor upgrade spec

--- test/e2e/frmwrk/cluster_upgrade_kubernetes_test.go
+++ test/e2e/frmwrk/cluster_upgrade_kubernetes_test.go
@@ -12,30 +12,36 @@ import (
 	capi_e2e "sigs.k8s.io/cluster-api/test/e2e"
 )
 
-var _ = Describe("Upgrade Kubernetes Cluster Version", Label("upgrade"), func() {
-	ctx := context.TODO()
-	fromKubernetesVersion := os.Getenv("KUBERNETES_VERSION_UPGRADE_FROM")
-	toKubernetesVersion := os.Getenv("KUBERNETES_VERSION_UPGRADE_TO")
+var _ = Describe("Upgrade Kubernetes Cluster Version", Ordered, Label("upgrade"), func() {
 
-	ec := e2eCtx.NewE2ECluster(ClusterConfig{
-		SpecName:                 "cluster-upgrade",
-		NamespaceName:            "cluster-upgrade",
-		ClusterName:              "cluster-upgrade",
-		KubernetesVersion:        fromKubernetesVersion,
-		ControlPlaneMachineImage: os.Getenv("E2E_CONTROL_PLANE_MACHINE_IMAGE_PREFIX") + fromKubernetesVersion,
-		ControlPlaneMachineCount: 1,
-		WorkerMachineImage:       os.Getenv("E2E_WORKER_MACHINE_IMAGE_PREFIX") + fromKubernetesVersion,
-		WorkerMachineCount:       1,
-	})
+	var (
+		ec                    *E2ECluster
+		ctx                   context.Context
+		fromKubernetesVersion string
+		toKubernetesVersion   string
+	)
 
-	ec.SetupMetalStackPreconditions(ctx)
-	ec.SetupNamespace(ctx)
+	BeforeAll(func() {
+		ctx = context.TODO()
+		fromKubernetesVersion = os.Getenv("KUBERNETES_VERSION_UPGRADE_FROM")
+		toKubernetesVersion = os.Getenv("KUBERNETES_VERSION_UPGRADE_TO")
 
-	DeferCleanup(func() {
-		ec.Dump(context.Background())
 	})
 
 	It(fmt.Sprintf("from %s to %s", fromKubernetesVersion, toKubernetesVersion), func() {
+		ec = e2eCtx.NewE2ECluster(ClusterConfig{
+			SpecName:                 "cluster-upgrade",
+			NamespaceName:            "cluster-upgrade",
+			ClusterName:              "cluster-upgrade",
+			KubernetesVersion:        fromKubernetesVersion,
+			ControlPlaneMachineImage: os.Getenv("E2E_CONTROL_PLANE_MACHINE_IMAGE_PREFIX") + fromKubernetesVersion,
+			ControlPlaneMachineCount: 1,
+			WorkerMachineImage:       os.Getenv("E2E_WORKER_MACHINE_IMAGE_PREFIX") + fromKubernetesVersion,
+			WorkerMachineCount:       1,
+		})
+		ec.SetupMetalStackPreconditions(ctx)
+		ec.SetupNamespace(ctx)
+
 		cfg := e2eCtx.E2EConfig.DeepCopy()
 		cfg.Variables["KUBERNETES_VERSION_UPGRADE_FROM"] = e2eCtx.envOrVar("KUBERNETES_VERSION_UPGRADE_FROM")
 		cfg.Variables["KUBERNETES_VERSION_UPGRADE_TO"] = e2eCtx.envOrVar("KUBERNETES_VERSION_UPGRADE_TO")
